895830165cdf6faaebd6c5afdd87f12743e2cf9a
Michael <mawoodcock@gmail.com>
2023-02-09 11:17:12 -0500

    Updated Data and HTML

--- test.js
+++ test.js
@@ -1,30 +1,37 @@
-## Import JS
-
-
-## Data
-var test_json = {
-    "Data": [
-    { "Name":"Amit Goenka" ,
-    "Major":"Physics" }, 
-    { "Name":"Smita Pallod" ,
-    "Major":"Chemistry" }, 
-    { "Name":"Rajeev Sen" , 
-    "Major":"Mathematics" }
+//Data
+var test_data = [
+      { "Item Name": "Test Event One",
+      "ItemStatus": "In Progress"
+      },
+      { "Item Name": "Test Event Two",
+      "ItemStatus": "To Do"
+      },
+      { "Item Name": "Test Event Three",
+      "ItemStatus": "To Do"
+      },
+      { "Item Name": "Test Event Four",
+      "ItemStatus": "Complete"
+      },
+      { "Item Name": "Test Event Five",
+      "ItemStatus": "In Progress"
+      },
+      { "Item Name": "Test Event Six",
+      "ItemStatus": "In Progress"
+      },
+      { "Item Name": "Test Event Seven",
+      "ItemStatus": "Complete"
+      }
  ]
-}
   
-## parse data
-
-var cities = [];
-
-var $this, input, text, obj;
-$('.line').each(function() { 
-   $this = $(this);
-   $input = $this.find("input");
-   text = $this.text();
-   obj = {};
-   obj[text] = $input.val();
-   cities.push(obj);
+// get count
+var counts = {};
+$.each(test_data,function(i,e) {
+   counts[this.ItemStatus] = (counts[this.ItemStatus] || 0) + 1;
 });
 
-users[0].cities = cities;
+console.log(counts)
+
+$("ul li").each(function(){
+   $(this).html(counts[$(this).attr("id")] || 0)
+   console.log($(this).attr("id"))
+})
